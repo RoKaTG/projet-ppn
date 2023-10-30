36265e05f8d412c2e5850a2f5fc990f9799d1415
RoKaTG <msiliniyassine@gmail.com>
2023-10-30 13:11:07 +0100

    Adding create_matrix function to create an empty matrix.

--- matrix/matrix.c
+++ matrix/matrix.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include "matrix.h"
 
-//#include <inttypes.h>
-//#include <stdbool.h>
+
+Matrix* create_matrix(int rows, int columns) {
+    Matrix* matrix = (Matrix*)malloc(sizeof(Matrix));
+    if (matrix != NULL) {
+        matrix->values = (double**)malloc(sizeof(double*));
+        matrix->rows = rows;
+        matrix -> columns = columns;
+        if (matrix->values != NULL) {
+            for(int i = 0; i < row; i++) {
+                matrix->values[i] = (double*)malloc(col * sizeof(double));
+            }
+        }
+    }
+    return matrix;
+}
 
 
