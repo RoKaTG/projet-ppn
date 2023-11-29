6d03af5296f594c31f590f5d55226f1788521c16
RKG <58750536+RoKaTG@users.noreply.github.com>
2023-11-29 22:58:36 +0100

    Adjusting create_matrix function.
    Adjusting create_matrix function that create an empty matrix

--- matrix/matrix.c
+++ matrix/matrix.c
@@ -1,22 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
 #include "matrix.h"
 
 
-Matrix* create_matrix(int rows, int columns) {
+Matrix* create_matrix(int r, int c) {
+    if (r <= 0 || c <= 0) {
+        return NULL;
+    }
     Matrix* matrix = (Matrix*)malloc(sizeof(Matrix));
-    if (matrix != NULL) {
-        matrix->values = (double**)malloc(sizeof(double*));
-        matrix->rows = rows;
-        matrix -> columns = columns;
-        if (matrix->values != NULL) {
-            for(int i = 0; i < row; i++) {
-                matrix->values[i] = (double*)malloc(col * sizeof(double));
+    if (matrix == NULL) {
+        return NULL;
+    }
+
+    matrix->row = r;
+    matrix->column = c;
+    matrix->value = (double**)malloc(r * sizeof(double*));
+
+    if (matrix->value == NULL) {
+        free(matrix);
+        return NULL;
+    }
+
+    for (int i = 0; i < r; i++) {
+        matrix->value[i] = (double*)malloc(c * sizeof(double));
+        if (matrix->value[i] == NULL) {
+            for (int j = 0; j < i; j++) {
+                free(matrix->value[j]);
             }
+            free(matrix->value);
+            free(matrix);
+            return NULL;
         }
     }
+
     return matrix;
 }
-
-
