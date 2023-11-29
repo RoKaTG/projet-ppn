aea2ffbbc62c7ebf7a97274b24752994a2cb3ec8
CHA-Khaled <146672809+CHA-Khaled@users.noreply.github.com>
2023-11-30 00:01:08 +0100

    Added test_check_dimensions.
    Added test_check_dimensions to test every cases of check_dimensions.

--- test/matrixTest.c
+++ test/matrixTest.c
@@ -58,9 +58,56 @@ static void test_print_matrix(void **state) {
 
 /******************************************/
 
+static void test_check_dimensions(void **state) {
+    // Test avec une matrice standard
+    Matrix *m1 = create_matrix(3, 3);
+    Size size1 = check_dimensions(m1);
+    assert_int_equal(size1.rows, 3);
+    assert_int_equal(size1.columns, 3);
+    assert_int_equal(size1.type, MATRIX_TYPE);
+
+    // Test avec un vecteur (une seule colonne)
+    Matrix *m2 = create_matrix(3, 1);
+    Size size2 = check_dimensions(m2);
+    assert_int_equal(size2.rows, 3);
+    assert_int_equal(size2.columns, 1);
+    assert_int_equal(size2.type, VECTOR_TYPE);
+
+    // Test avec une autre matrice standard
+    Matrix *m3 = create_matrix(2, 4);
+    Size size3 = check_dimensions(m3);
+    assert_int_equal(size3.rows, 2);
+    assert_int_equal(size3.columns, 4);
+    assert_int_equal(size3.type, MATRIX_TYPE);
+
+    // Test avec un vecteur (ligne unique)
+    Matrix *m4 = create_matrix(1, 5);
+    Size size4 = check_dimensions(m4);
+    assert_int_equal(size4.rows, 1);
+    assert_int_equal(size4.columns, 5);
+    assert_int_equal(size4.type, MATRIX_TYPE);  // Ligne unique est considérée comme une matrice
+
+    // Test avec une matrice vide ou NULL
+    Matrix *m5 = NULL;
+    Size size5 = check_dimensions(m5);
+    assert_int_equal(size5.rows, 0);
+    assert_int_equal(size5.columns, 0);
+    assert_int_equal(size5.type, MATRIX_TYPE);
+
+    // Nettoyage
+    free_matrix(&m1);
+    free_matrix(&m2);
+    free_matrix(&m3);
+    free_matrix(&m4);
+    // Pas besoin de libérer m5 car il est NULL
+}
+
+/******************************************/
+
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_print_matrix),
+        cmocka_unit_test(test_check_dimensions),
     };
     
     return cmocka_run_group_tests(tests, NULL, NULL);
