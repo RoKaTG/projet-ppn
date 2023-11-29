1d23e11750db5d5aabb24275d9393e7fed46ea8b
Arhab Sofiane <145778662+dxhardys@users.noreply.github.com>
2023-11-30 00:08:30 +0100

    Added copy_matrix function.
    Added copy_matrix function that copy a matrix (size, values, pointer etc) into a new one.

--- matrix/matrix.c
+++ matrix/matrix.c
@@ -19,4 +19,23 @@ Matrix* create_matrix(int rows, int columns) {
     return matrix;
 }
 
+Matrix* copy_matrix(Matrix *original) {
+    if (original == NULL) {
+        return NULL;  // Retourne NULL si la matrice originale est NULL
+    }
 
+    // Créer une nouvelle matrice avec les mêmes dimensions que l'originale
+    Matrix* new_matrix = create_matrix(original->row, original->column);
+    if (new_matrix == NULL) {
+        return NULL;  // Retourne NULL si l'allocation de mémoire échoue
+    }
+
+    // Copier les valeurs de l'originale dans la nouvelle matrice
+    for (int i = 0; i < original->row; i++) {
+        for (int j = 0; j < original->column; j++) {
+            new_matrix->value[i][j] = original->value[i][j];
+        }
+    }
+
+    return new_matrix;
+}
