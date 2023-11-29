d350e84767da736b7be54e6f32ef42e4fd15c2bf
RoKaTG <msiliniyassine@gmail.com>
2023-11-29 21:45:19 +0100

    Reajusting matrix's header.
    Header for matrix basics function was totally wrongly written.

--- matrix/matrix.h
+++
@@ -1,9 +0,0 @@
-typedef struct {
-    int rows;
-    int columns;
-    double** values;
- } Matrix;
-
-Matrix* create_matrix(int rows, int columns);
-void fill_matrix(Matrix *matrix, double value[]);  //-> for i -> ligne for j -> colonne
-void print_matrix(Matrix *matrix);
