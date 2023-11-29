80a68fc77a4bcca89e2f61f5bf3617095b136737
RKG <58750536+RoKaTG@users.noreply.github.com>
2023-11-29 22:56:23 +0100

    Added test_create_matrix.
    Added test_creation_matrix function to test every cases of create_matrix.

--- test/matrixTest.c
+++ test/matrixTest.c
@@ -1,7 +1,83 @@
+#include <stdint.h>
+#include <setjmp.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <cmocka.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-//#include <inttypes.h>
-//#include <stdbool.h>
+//#include "../matrix/matrix.c"
+#include "../matrix/matrix.h"
 
+static void test_create_matrix(void **state) {
+    Matrix *m = create_matrix(10, 10);
+    assert_non_null(m);
+    assert_int_equal(m->row, 10);
+    assert_int_equal(m->column, 10);
 
+    for (int i = 0; i < 10; i++) {
+        assert_non_null(m->value[i]);
+    }
+
+    // Libère la mémoire après le test
+    for (int i = 0; i < 10; i++) {
+        free(m->value[i]);
+    }
+    free(m->value);
+    free(m);
+
+/******************************************/    
+
+    Matrix *m2 = create_matrix(1000, 1000);
+    assert_non_null(m2);
+    assert_int_equal(m2->row, 1000);
+    assert_int_equal(m2->column, 1000);
+
+    for (int i = 0; i < 1000; i++) {
+        assert_non_null(m2->value[i]);
+    }
+
+    // Libère la mémoire après le test
+    for (int i = 0; i < 1000; i++) {
+        free(m2->value[i]);
+    }
+    free(m2->value);
+    free(m2);
+
+/******************************************/    
+
+    Matrix *m3 = create_matrix(10000, 10000);
+    assert_non_null(m3);
+    assert_int_equal(m3->row, 10000);
+    assert_int_equal(m3->column, 10000);
+
+    for (int i = 0; i < 10000; i++) {
+        assert_non_null(m3->value[i]);
+    }
+
+    // Libère la mémoire après le test
+    for (int i = 0; i < 10000; i++) {
+        free(m3->value[i]);
+    }
+    free(m3->value);
+    free(m3);
+
+/******************************************/    
+
+    assert_null(create_matrix(0, 0));
+    assert_null(create_matrix(1, 0));
+    assert_null(create_matrix(0, 1));
+    assert_null(create_matrix(-1, 1));
+    assert_null(create_matrix(1, -1));
+    assert_null(create_matrix(-1, -1));
+}
+
+/******************************************/
+
+int main(void) {
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(test_create_matrix),
+    };
+    
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
