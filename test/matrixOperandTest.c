70489ecf66fc2d1231aa33da5c8af40d188a7a30
RKG <58750536+RoKaTG@users.noreply.github.com>
2023-11-30 18:36:10 +0100

    Structuring the test file
    Structuring the future tests of matrix's operand to support cmocka testing (and soon coverage).

--- test/matrixOperandTest.c
+++ test/matrixOperandTest.c
@@ -1,7 +1,21 @@
+#include <stdint.h>
+#include <setjmp.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <cmocka.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-//#include <inttypes.h>
-//#include <stdbool.h>
+#include "../matrix_operand/matrixOperand.h"
+
+/******************************************/    
+
+int main(void) {
+    const struct CMUnitTest tests[] = {
+    };
+    
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
 
 
