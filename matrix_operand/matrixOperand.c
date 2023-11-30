bb2f27d53545bdd99a9796eed930eb6f1d3981b6
rrriad <145778442+rrriad@users.noreply.github.com>
2023-11-30 19:50:54 +0100

    Added scale_matrix function
    Added scale_matrix function that is used to scale a matrix with a scalar.

--- matrix_operand/matrixOperand.c
+++ matrix_operand/matrixOperand.c
@@ -3,3 +3,13 @@
 #include <math.h>
 
 #include "matrixOperand.h"
+
+void scale_matrix(Matrix* matrix, double scalar) {
+    if (matrix == NULL) return;
+
+    for (int i = 0; i < matrix->row; i++) {
+        for (int j = 0; j < matrix->column; j++) {
+            matrix->value[i][j] *= scalar;
+        }
+    }
+}
