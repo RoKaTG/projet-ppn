eca562586e787b7ef5931ffbf63a6e94204c9fc2
RKG <58750536+RoKaTG@users.noreply.github.com>
2023-11-30 19:26:43 +0100

    Added add_matrix function
    Added add_matrix function that allow to addition two matrix.

--- matrix_operand/matrixOperand.c
+++ matrix_operand/matrixOperand.c
@@ -3,3 +3,22 @@
 #include <math.h>
 
 #include "matrixOperand.h"
+
+Matrix* add_matrix(Matrix* matrix1, Matrix* matrix2) {
+    if (matrix1 == NULL || matrix2 == NULL || matrix1->row != matrix2->row || matrix1->column != matrix2->column) {
+        return NULL;
+    }
+
+    Matrix* result = create_matrix(matrix1->row, matrix1->column);
+    if (result == NULL) {
+        return NULL;
+    }
+
+    for (int i = 0; i < matrix1->row; i++) {
+        for (int j = 0; j < matrix1->column; j++) {
+            result->value[i][j] = matrix1->value[i][j] + matrix2->value[i][j];
+        }
+    }
+
+    return result;
+}
