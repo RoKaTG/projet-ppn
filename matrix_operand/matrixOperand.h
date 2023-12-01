751310407d879f75e7dec30cb36793950631e2a0
CHA-Khaled <146672809+CHA-Khaled@users.noreply.github.com>
2023-12-01 10:25:21 +0100

    Added dotprod declaration

--- matrix_operand/matrixOperand.h
+++ matrix_operand/matrixOperand.h
@@ -7,5 +7,6 @@ void scale_matrix(Matrix* matrix, double scalar);
 Matrix* inverse_matrix(Matrix* matrix);
 Matrix* add_matrix(Matrix* matrix1, Matrix* matrix2);
 Matrix* sub_matrix(Matrix* matrix1, Matrix* matrix2);
+Matrix* dotprod(Matrix *matrix, Matrix *vector);
 
 #endif //MATRIXOPERAND_H
