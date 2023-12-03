4d40d46621b4b6789272407207c909b8c76e2ace
RKG <58750536+RoKaTG@users.noreply.github.com>
2023-12-03 21:51:53 +0100

    Added sigmoid function
    Added sigmoid function that limit the output between 0 & 1 for probabilities output perfect of the MNIST output required for the project.

--- neural_network/neuralNetwork.c
+++ neural_network/neuralNetwork.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
-//#include <inttypes.h>
-//#include <stdbool.h>
+#include "../mnist_reader/mnist_reader.h"
+#include "neural_network.h"
 
-
+double sigmoid(double x) {
+    return 1.0 / (1.0 + exp(-x));
+}
