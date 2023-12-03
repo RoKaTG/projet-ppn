e761bca914aa94eb355cbce6440627534085280a
RKG <58750536+RoKaTG@users.noreply.github.com>
2023-12-03 21:53:52 +0100

    Added apply_function function
    Added apply_function function that apply the activation function which is sigmoid to each output's matrix

--- neural_network/neuralNetwork.c
+++ neural_network/neuralNetwork.c
@@ -9,3 +9,11 @@
 double sigmoid(double x) {
     return 1.0 / (1.0 + exp(-x));
 }
+
+void apply_function(Matrix* m, double (*func)(double)) {
+    for (int i = 0; i < m->row; i++) {
+        for (int j = 0; j < m->column; j++) {
+            m->value[i][j] = func(m->value[i][j]);
+        }
+    }
+}
