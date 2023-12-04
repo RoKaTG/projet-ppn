a881740989a280e3397a9b24a798d1412b85eff3
rrriad <145778442+rrriad@users.noreply.github.com>
2023-12-04 09:31:11 +0100

    Added calcul_derivee_numerique function
    Added calcul_derivee_numerique function to calculate the numeric derivability of a function (here x²)

--- gradient.c
+++ gradient.c
@@ -4,3 +4,7 @@
 double fonction(double x) {
     return x * x;
 }
+
+double calculer_derivee_numerique(double (*fonction)(double), double x, double h) {
+    return (fonction(x + h) - fonction(x)) / h;
+}
