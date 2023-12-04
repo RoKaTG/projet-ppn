13534d78e6d3967fcb2f5f81fb8879355ff487de
rrriad <145778442+rrriad@users.noreply.github.com>
2023-12-04 09:32:36 +0100

    Added descente_gradient function & main
    Added descente_gradient function & main to test the gradient in a simple function such as x²

--- gradient.c
+++ gradient.c
@@ -8,3 +8,23 @@ double fonction(double x) {
 double calculer_derivee_numerique(double (*fonction)(double), double x, double h) {
     return (fonction(x + h) - fonction(x)) / h;
 }
+
+void descente_gradient(double taux_apprentissage, int iterations, double h) {
+    double x = 0.0;
+    for (int i = 0; i < iterations; ++i) {
+        double gradient = calculer_derivee_numerique(fonction, x, h); 
+        x = x - taux_apprentissage * gradient;
+        printf("ItÃ©ration %d : x = %lf, f(x) = %lf\n", i + 1, x, fonction(x));
+    }
+}
+
+int main() {
+    double taux_apprentissage = 0.1; 
+    int iterations = 100; 
+    double h = 0.0001; 
+
+    descente_gradient(taux_apprentissage, iterations, h);
+
+    return 0;
+}
+
