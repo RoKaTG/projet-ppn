07dac5777b88b1c55c2a144006a9f62385f54d7b
RKG <58750536+RoKaTG@users.noreply.github.com>
2023-12-05 15:17:03 +0100

    Added backpropagate function
    Added backpropagate function that use a simple backpropagation for one layer network.
    
    
    Co-authored-by: Sofiane <145778662+dxhardys@users.noreply.github.com>
    Co-authored-by: Khaled <146672809+CHA-Khaled@users.noreply.github.com>
    Co-authored-by: Riad <145778442+rrriad@users.noreply.github.com>

--- neural_network/neuralNetwork.c
+++ neural_network/neuralNetwork.c
@@ -3,8 +3,9 @@
 #include <math.h>
 #include <time.h>
 
+#include "../matrix/matrix.h"
+#include "../matrix_operand/matrixOperand.h"
 #include "../mnist_reader/mnist_reader.h"
-#include "neural_network.h"
 
 double sigmoid(double x) {
     return 1.0 / (1.0 + exp(-x));
@@ -15,5 +16,18 @@ void apply_function(Matrix* m, double (*func)(double)) {
         for (int j = 0; j < m->column; j++) {
             m->value[i][j] = func(m->value[i][j]);
         }
+    }
+}
+
+void backpropagate(Matrix* input, Matrix* output, Matrix* expected, Matrix* weights, Matrix* biases, double learning_rate) {
+    double error = expected->value[0][0] - output->value[0][0];
+    double derivative = output->value[0][0] * (1 - output->value[0][0]);
+    double gradient = error * derivative;
+    
+    for (int i = 0; i < weights->row; i++) {
+        for (int j = 0; j < weights->column; j++) {
+            weights->value[i][j] += learning_rate * gradient * input->value[j][0];
+        }
+        biases->value[i][0] += learning_rate * gradient;
     }
 }
