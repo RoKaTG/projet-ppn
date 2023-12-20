e017f15ed9b816d8f33f4cf1d1d6eebf0ae6e765
RoKaTG <msiliniyassine@gmail.com>
2023-12-20 13:23:04 +0100

    Creating future file for n-layer network + renaming 1-layer network

---
+++ neural_network/neuralNetwork.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "neuralNetwork.h"
+#include "../mnist_reader/mnist_reader.h"
+#include "../matrix_operand/matrixOperand.h"
+
+
+int main() {
+    return 0;
+}
---
+++ neural_network/neuralNetwork.h
@@ -0,0 +1,23 @@
+#ifndef NEURAL_NETWORK_H
+#define NEURAL_NETWORK_H
+
+#include "../matrix/matrix.h"
+
+typedef struct {
+    Matrix* weights;       // Matrice des poids
+    Matrix* biases;        // Vecteur des biais
+    Matrix* outputs;       // Sorties de la couche
+    Matrix* inputs;
+    Matrix* deltas;        // Deltas pour la rétropropagation
+    double (*activation_function)(double); // Fonction d'activation
+    double (*activation_function_derivative)(double); // Dérivée de la fonction d'activation
+} Layer;
+
+typedef struct {
+    int number_of_layers; // Nombre de couches
+    Layer** layers;       // Tableau des couches
+} NeuralNetwork;
+
+
+#endif // NEURAL_NETWORK_H
+
