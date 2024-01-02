f5d024d7fb57123ddfaf7d8d1d11dbc68f94c5ef
CHA-Khaled <146672809+CHA-Khaled@users.noreply.github.com>
2024-01-02 10:53:06 +0100

    Added sigmoid function
    Added sigmoid function as an activation function for the network.

--- neural_network/neuralNetwork.c
+++ neural_network/neuralNetwork.c
@@ -9,6 +9,9 @@
 #include "../mnist_reader/mnist_reader.h"
 #include "../matrix_operand/matrixOperand.h"
 
+double sigmoid(double x) {
+    return 1.0 / (1.0 + exp(-x));
+}
 
 int main() {
     return 0;
