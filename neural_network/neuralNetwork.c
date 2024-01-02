e9d97effce4cebaebc75a350d1529d44db90f248
rrriad <145778442+rrriad@users.noreply.github.com>
2024-01-02 10:59:01 +0100

    Added apply_function_derivative function
    Added apply_function_derivative function that apply the derivative of the activation function across the network.

--- neural_network/neuralNetwork.c
+++ neural_network/neuralNetwork.c
@@ -17,6 +17,14 @@ void apply_function(Matrix* m, double (*func)(double)) {
     }
 }
 
+void apply_function_derivative(Matrix* m, double (*func)(double)) {
+    for (int i = 0; i < m->row; i++) {
+        for (int j = 0; j < m->column; j++) {
+            m->value[i][j] = func(m->value[i][j]);
+        }
+    }
+}
+
 int main() {
     return 0;
 }
