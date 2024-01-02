74be2d8c278971bcf493578b3504992b6c037c37
CHA-Khaled <146672809+CHA-Khaled@users.noreply.github.com>
2024-01-02 11:24:30 +0100

    Rearranging function structure & order

--- neural_network/neuralNetwork.c
+++ neural_network/neuralNetwork.c
@@ -9,21 +9,6 @@
 #include "../mnist_reader/mnist_reader.h"
 #include "../matrix_operand/matrixOperand.h"
 
-void apply_function(Matrix* m, double (*func)(double)) {
-    for (int i = 0; i < m->row; i++) {
-        for (int j = 0; j < m->column; j++) {
-            m->value[i][j] = func(m->value[i][j]);
-        }
-    }
-}
-
-void apply_function_derivative(Matrix* m, double (*func)(double)) {
-    for (int i = 0; i < m->row; i++) {
-        for (int j = 0; j < m->column; j++) {
-            m->value[i][j] = func(m->value[i][j]);
-        }
-    }
-}
 
 double sigmoid(double x) {
     return 1.0 / (1.0 + exp(-x));
@@ -42,6 +27,22 @@ void softmax(Matrix* m) {
     }
     for (int i = 0; i < m->row; i++) {
         m->value[i][0] /= sum;
+    }
+}
+
+void apply_function(Matrix* m, double (*func)(double)) {
+    for (int i = 0; i < m->row; i++) {
+        for (int j = 0; j < m->column; j++) {
+            m->value[i][j] = func(m->value[i][j]);
+        }
+    }
+}
+
+void apply_function_derivative(Matrix* m, double (*func)(double)) {
+    for (int i = 0; i < m->row; i++) {
+        for (int j = 0; j < m->column; j++) {
+            m->value[i][j] = func(m->value[i][j]);
+        }
     }
 }
 
