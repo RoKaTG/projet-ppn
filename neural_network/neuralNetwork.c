1c57c9c2a66d57ed9b9301f6829bbd8f230e0ef4
Arhab Sofiane <145778662+dxhardys@users.noreply.github.com>
2024-01-02 11:01:39 +0100

    Added softmax function
    Added softmax function which is an activation function that allow us to converts a vector of K real numbers into a probability distribution of K possible outcomes.

--- neural_network/neuralNetwork.c
+++ neural_network/neuralNetwork.c
@@ -9,6 +9,16 @@
 #include "../mnist_reader/mnist_reader.h"
 #include "../matrix_operand/matrixOperand.h"
 
+void softmax(Matrix* m) {
+    double sum = 0.0;
+    for (int i = 0; i < m->row; i++) {
+        m->value[i][0] = exp(m->value[i][0]);
+        sum += m->value[i][0];
+    }
+    for (int i = 0; i < m->row; i++) {
+        m->value[i][0] /= sum;
+    }
+}
 
 int main() {
     return 0;
