be6497a2371eaf7e086c9a30fb882a500613a6fe
RoKaTG <msiliniyassine@gmail.com>
2024-02-03 15:05:08 +0100

    Refactor matrix_operand

---
+++ matrix/matrixOperand.h
@@ -0,0 +1,15 @@
+#ifndef MATRIXOPERAND_H
+#define MATRIXOPERAND_H
+
+#include "../matrix/matrix.h"
+
+void scale_matrix(Matrix* matrix, double scalar);
+Matrix* inverse_matrix(Matrix* matrix);
+Matrix* add_matrix(Matrix* matrix1, Matrix* matrix2);
+Matrix* sub_matrix(Matrix* matrix1, Matrix* matrix2);
+Matrix* dotprod(Matrix *matrix, Matrix *vector);
+bool compare_matrix(Matrix* matrix1, Matrix* matrix2);
+Matrix* dgemm(Matrix* matrix1, Matrix* matrix2);
+Matrix* transpose_matrix(Matrix* matrix1);
+
+#endif //MATRIXOPERAND_H
