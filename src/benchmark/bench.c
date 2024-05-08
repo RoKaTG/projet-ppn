a438c433b3fd593debeeae212a848e443b0357a5
RoKaTG <msiliniyassine@gmail.com>
2024-05-08 14:50:56 +0200

    Minor FIX[COMM]

--- src/benchmark/bench.c
+++ src/benchmark/bench.c
@@ -1,5 +1,4 @@
 #include <cblas.h>
-#include <clapack.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -10,6 +9,21 @@
 
 #include "../../include/benchmark/bench.h"
 
+/**************************************/
+/*                                    */
+/*       Benchmarking results         */
+/*                                    */
+/**************************************/
+
+/**
+ * Print benchmark results for a computational routine.
+ *
+ * This function prints benchmark results including routine name, topology, activation function,
+ * number of training images, epochs, batch size, precision, total time taken, average epoch time,
+ * and error rate.
+ *
+ * @param result Pointer to a Benchmark struct containing the benchmark results to be printed.
+ */
 void printBenchmarkResult(const Benchmark *result) {
     printf("%-10s %-20s %-10s %-20d %-10d %-15d %-15.2f %-20.2f %-20.2f %-15.2f\n", 
         result->routine, result->topology, result->actFunction, result->trainingImages, 
