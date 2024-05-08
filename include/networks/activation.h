77d03d73bef53375fec952ca9a98373a22e985ab
RoKaTG <msiliniyassine@gmail.com>
2024-05-08 14:15:40 +0200

    Header of activation function

--- include/networks/activation.h
+++ include/networks/activation.h
@@ -11,6 +11,15 @@ double sigmoidPrime(double x);
 
 /**************************************/
 /*                                    */
+/*      Fast Sigmoid's headers        */
+/*                                    */
+/**************************************/
+
+double fast_sigmoid(double x);
+double fast_sigmoidPrime(double x);
+
+/**************************************/
+/*                                    */
 /*          Softmax's header          */
 /*                                    */
 /**************************************/
@@ -28,6 +37,48 @@ double reluPrime(double x);
 
 /**************************************/
 /*                                    */
+/*        leakyreLu's header          */
+/*                                    */
+/**************************************/
+
+double leakyReLU(double x, double alpha);
+double leakyReLUPrime(double x, double alpha);
+
+/**************************************/
+/*                                    */
+/*          Swish's header            */
+/*                                    */
+/**************************************/
+
+double swish(double x);
+double swishPrime(double x);
+
+/**************************************/
+/*                                    */
 /*          tanh's header             */
 /*                                    */
 /**************************************/
+
+double tanhh(double x);
+double tanhPrime(double x);
+
+/**************************************/
+/*                                    */
+/*          AVX2's header             */
+/*                                    */
+/**************************************/
+
+/******************************reLu******************************/
+
+void relu_avx2(double *x, double *output, int length);
+void reluPrime_avx2(double *x, double *output, int length);
+
+/******************************leakyreLu******************************/
+
+void leakyReLU_avx2(double *x, double *output, int length, double alpha);
+void leakyReLUPrime_avx2(double *x, double *output, int length, double alpha);
+
+/******************************fastSigmoid******************************/
+
+void fast_sigmoid_avx2(double *x, double *output, int length);
+void fast_sigmoidPrime_avx2(double *x, double *output, int length);
